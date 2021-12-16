bb7f5d55ca39140c951c9ba92ecd75357bfb438b
CHRIS SIFUNDZA <chrissfundza@gmail.com>
2021-12-16 18:01:09 +0200

    make it better

--- public/script.js
+++ public/script.js
@@ -5,11 +5,9 @@ myVideo.muted = true;
 let configuration = {
 	iceServers: [
 		{
-			urls: [
-				"stun:stun.l.google.com:19302",
-				"stun:stun1.l.google.com:19302",
-				"stun:stun2.l.google.com:19302",
-			],
+			url: "turn:numb.viagenie.ca",
+			credential: "muazkh",
+			username: "webrtc@live.com",
 		},
 	],
 };
